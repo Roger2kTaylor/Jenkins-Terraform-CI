a9305e5793c154951b3bac8d861b95ff13e417bd
Roger2kTaylor <roger@skye-nets.com>
2021-05-12 01:59:33 -0400

    Remote Backend

--- terraform.tf
+++ terraform.tf
@@ -7,3 +7,13 @@ terraform {
   required_version = ">= 0.12, <= 0.15.3"
 }
 
+
+# TERRAFORM REMOTE BACKEND
+terraform {
+  backend "s3" {
+    bucket = "skye-remote-backend"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
